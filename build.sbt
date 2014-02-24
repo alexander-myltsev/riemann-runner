92d5c5c17adfb29d0877424da6a9c3d1980bd66c
Alexander Myltsev <alexander.myltsev@phystech.edu>
2014-02-24 22:41:46 +0400

    Update to `riemann-server 0.2.5`
    Add `riemann-java-client`

--- build.sbt
+++ build.sbt
@@ -1,1 +1,12 @@
 name := "riemann-runner"
+
+resolvers ++= Seq(
+  "collective" at "http://nexus.collective-media.net/content/repositories/thirdparty/",
+  "clojars" at "https://clojars.org/repo/"
+)
+
+libraryDependencies ++= Seq(
+  "riemann"              % "riemann"               % "0.2.5-SNAPSHOT",
+  "com.aphyr"            % "riemann-java-client"   % "0.2.8" excludeAll (
+  ExclusionRule(organization = "com.yammer.metrics"))
+)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,28 @@
+import com.aphyr.riemann.client.RiemannClient
+import java.util.concurrent.Executors
+import scala.concurrent.{ExecutionContext, Future}
+
 object Main extends App {
-  riemann.bin.main(Array("./etc/riemann.conf"))
+  private[this] val blockingIOThreadPool = Executors.newFixedThreadPool(1)
+  private[this] val blockingIOEC = ExecutionContext.fromExecutorService(blockingIOThreadPool)
+
+  Future {
+    riemann.bin_runner.main(Array("./etc/riemann.config"))
+  }(blockingIOEC)
+
+  Thread.sleep(5000)
+
+  val client = RiemannClient.tcp("localhost", 5555)
+  client.connect()
+  client.event().
+    service("fridge").
+    state("running").
+    metric(5.123).
+    tags("appliance", "cold").
+    send()
+
+  val result = client.query("tagged \"cold\" and metric > 0")
+  client.disconnect()
+
+  println(result)
 }
